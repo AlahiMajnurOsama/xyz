4fa6e90f36b678192d5664ce3d2a7e7870cc86a2
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-03-30 00:20:25 -0700

    Update passwd.sh

--- passwd.sh
+++ passwd.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-echo "Changing admin password..."
+echo "Changing root password and all user passwords..."
 echo "New password: 726268"
 
-# Use sudo to change the password for the root user
+# Use sudo to change the root password
 echo "root:726268" | sudo chpasswd
 
-echo "Password changed successfully!"
+# Get a list of all non-system users on the system
+users=$(getent passwd | awk -F: '$3 >= 1000 && $1 != "nobody" { print $1 }')
+
+# Loop through each user and change their password using sudo
+for user in $users
+do
+  echo "$user:726268" | sudo chpasswd
+done
+
+echo "Passwords changed successfully!"
