021af054c6ec90cf089d571362a88ecd181bbb36
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-03-30 12:00:41 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ apt-get install sudo -y
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo chmod +x xyz/passwd.sh
-sudo chmod +x xyz/nc.sh
+sudo chmod +x xyz/ngrok.sh
 sudo chmod +x xyz/install.sh
 sudo bash xyz/passwd.sh
-sudo bash xyz/nc.sh
+sudo bash xyz/ngrok.sh
