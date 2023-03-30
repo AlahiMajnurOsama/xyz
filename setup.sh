2acfc2b0d5e696d86b0b928d641aa4b1a2f74bb4
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-03-30 11:54:42 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@ apt-get install sudo -y
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo chmod +x xyz/passwd.sh
-sudo chmod +x xyz/install.sh
 sudo chmod +x xyz/nc.sh
 sudo bash xyz/passwd.sh
-sudo bash xyz/install.sh
 sudo bash xyz/nc.sh
