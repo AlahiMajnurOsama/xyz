322bdc158f0781e7db198d03b376ed23f2a83bfe
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-03-29 23:17:01 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Install OpenSSH server and set it up to listen on port 22
 sudo apt-get update
 sudo apt-get install -y openssh-server
-
+sudo apt-get install -y unzip
 
 sudo sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
 sudo systemctl restart sshd
