e0034abc1661b6a0479a70a0db0743ed101295de
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-03-29 23:48:09 -0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -11,5 +11,5 @@ unzip ngrok-stable-linux-amd64.zip
 sudo mv ngrok /usr/local/bin/ngrok
 
 # Set up Ngrok to forward port 22
-sudo ngrok authtoken 2NhYBtrkboycOcSc6hxueEykTeq_7Zg2LQtUQe61GQM3z5sYG
+sudo ngrok authtoken 2Ne1wOGAXg3RTt5Cu0EpmU1EZfx_3bM44DaPgHRJACLN7hbwf
 sudo ngrok tcp 22 --log ".ngrok.log" 
