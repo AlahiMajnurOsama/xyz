e173ef8f0142398c9cd0c1da361be967e33927af
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-03-29 23:37:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# Install OpenSSH server and set it up to listen on port 22
-sudo apt-get update
-sudo apt-get install -y openssh-server
 
-sudo sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+
+# Update the package index
+sudo apt update
+
+# Install Nginx and OpenSSH server
+sudo apt install -y nginx openssh-server
+
+# Start Nginx and OpenSSH server
+sudo systemctl start nginx
+sudo systemctl start ssh
+
+# Enable Nginx and OpenSSH server to start automatically on boot
+sudo systemctl enable nginx
+sudo systemctl enable ssh
 
 
 # Install additional packages
