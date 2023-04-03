9f645276c1a839fa633d5fe437db5ec0b0dcd10e
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-04-03 00:35:49 -0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -13,10 +13,4 @@ sudo mv ngrok /usr/local/bin/ngrok
 
 # Set up Ngrok to forward port 22
 sudo ngrok authtoken 2Ne1wOGAXg3RTt5Cu0EpmU1EZfx_3bM44DaPgHRJACLN7hbwf
-ngrok_cmd="sudo ngrok tcp 22 --log \".ngrok.log\" &> /dev/null &"
 
-# Start a new screen session and execute the Ngrok command in it
-screen -S ngrok -d -m bash -c "${ngrok_cmd}"
-
-# Print confirmation message
-echo "Ngrok tunnel started in a new screen session."
