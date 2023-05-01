297f3033863b37a6f5f16401b5d819b50ce3a81c
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-05-01 09:18:28 -0700

    Create gui.sh

---
+++ gui.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Update the package list and upgrade existing packages
+sudo apt update && sudo apt upgrade -y
+
+# Install the XFCE4 desktop environment and its goodies
+sudo apt install xfce4 xfce4-goodies -y
+
+# Install a VNC server
+sudo apt install tightvncserver -y
+
+# Set up the VNC server to start at boot and use XFCE4 as the default desktop environment
+sudo bash -c 'cat << EOF > /etc/systemd/system/vncserver@.service
+[Unit]
+Description=Remote desktop service (VNC)
+After=syslog.target network.target
+
+[Service]
+Type=forking
+User=root
+Group=root
+WorkingDirectory=/root
+PIDFile=/root/.vnc/%H:%i.pid
+ExecStartPre=-/usr/bin/vncserver -kill :%i > /dev/null 2>&1
+ExecStart=/usr/bin/vncserver -depth 24 -geometry 1280x800 -localhost :%i
+ExecStop=/usr/bin/vncserver -kill :%i
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+# Enable and start the VNC server
+sudo systemctl daemon-reload
+sudo systemctl enable vncserver@1.service
+sudo systemctl start vncserver@1.service
+
+# Set a VNC password for the root user
+echo "Setting VNC password for root user..."
+sudo -u root echo -e "726268\n726268" | vncpasswd
+
+# Restart the VNC server to apply the password
+sudo systemctl restart vncserver@1.service
+
+echo "Setup complete. You can now connect to the XFCE4 desktop environment via VNC on port 5901."
